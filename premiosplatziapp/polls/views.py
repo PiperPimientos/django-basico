f7e2fcf466761ee5ef5923e913093710d5ec0448
piperportatil <feliperestrepomeneses@gmail.com>
2022-02-14 17:49:13 -0500

    agregamos los views de details, results y vote a nuestra app polls

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello World")
-
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Estas en la pagina principal de Premios Platzi App")
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta numero {question_id}")
